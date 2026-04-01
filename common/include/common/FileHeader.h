b0539393fda626c9856a76eea54088e007268db3
Franciszek Wójcik <f.wojcikk@gmail.com>
2026-04-01 17:53:22 +0200

    changed padding

--- common/include/common/FileHeader.h
+++ common/include/common/FileHeader.h
@@ -7,6 +7,6 @@ struct FileHeader {
     uint32_t version;
     uint32_t signalCount;
     SignalDescriptor signals[128];
-    uint8_t padding[49152]; // will need to check if this is correct
+    uint8_t padding[54644]; // will need to check if this is correct
 };
 #pragma pack(pop)

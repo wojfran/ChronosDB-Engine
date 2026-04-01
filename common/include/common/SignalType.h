cf2a8c61fc63f3f30bdf6df8fa07d9bd93fd9504
Franciszek Wójcik <f.wojcikk@gmail.com>
2026-04-01 17:37:24 +0200

    Correction to signaltype

--- common/include/common/SignalType.h
+++ common/include/common/SignalType.h
@@ -1,4 +1,10 @@
-enum class SignalType {
+#include <cstdint>
+
+// apparently enum class takes up 
+// the same amount of space as int (4 bytes) 
+// by default. By extending it from uint8_t 
+// we save 3 bytes :)
+enum class SignalType : uint8_t {
     Double = 0,
     Float = 1,
     int32 = 2,

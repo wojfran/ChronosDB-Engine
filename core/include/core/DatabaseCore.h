8006e1100f0f01e5b4fc10592e7d0048e6bdc2b8
Franciszek Wójcik <f.wojcikk@gmail.com>
2026-04-16 16:42:50 +0200

    Changed the definition of databasecore class

--- core/include/core/DatabaseCore.h
+++ core/include/core/DatabaseCore.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <mutex>
 #include "common/SignalType.h"
 #include "common/Sample.h"
 
@@ -19,13 +20,20 @@ class DatabaseCore {
     std::unique_ptr<StorageManager> m_storage;
     std::unique_ptr<IndexProvider> m_index;
     std::unordered_map<uint32_t, std::unique_ptr<SignalBase>> m_signals;
+    mutable std::mutex m_dbMutex;
+
+    void loadSignalFromHeader();
+    void rebuildState();
 
     public:
-    DatabaseCore();
+    DatabaseCore(uint32_t indexInterval = 100, size_t maxIndexExntires = 1000);
     ~DatabaseCore();
     bool open(const std::string& path);
-    bool addSignal(std::string name, std::string unit, SignalType type);
+    void close();
+    bool addSignal(uint32_t id, std::string name, std::string unit, SignalType type);
     void append(uint32_t id, double value, uint8_t status = 0);
-    const SignalBase& queryStats(uint32_t id, int64_t t1, int64_t t2) const;
-    std::vector<Sample> getRange(uint32_t id, int64_t t1, int64_t t2) const;
+    const SignalBase* getGlobalStats(uint32_t id) const;
+    std::unique_ptr<SignalBase> getStatsInRange(uint32_t id, int64_t t1, int64_t t2);
+    std::vector<Sample> getRange(uint32_t id, int64_t t1, int64_t t2);
+    bool isOpen() const;
 };
